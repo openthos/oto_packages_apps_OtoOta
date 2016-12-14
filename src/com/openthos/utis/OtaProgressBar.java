7ab2ecc7d17483ff09124681b28bb75cd328f32e
Zeng Feifei <372442604@qq.com>
2016-12-14 01:02:59 +0000

    OtoOta: Add new file to fix building issue
    Signed-off-by: Zeng Feifei <372442604@qq.com>
    Signed-off-by: Chen Gang <chengang@emindsoft.com.cn>

---
+++ src/com/openthos/utis/OtaProgressBar.java
@@ -0,0 +1,59 @@
+package com.openthos.utis;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+public class OtaProgressBar extends ProgressBar {
+    private String mText;
+    private Paint mPaint;
+
+    public OtaProgressBar(Context context) {
+        super(context);
+        initText();
+    }
+
+    public OtaProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initText();
+    }
+
+
+    public OtaProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initText();
+    }
+
+    public synchronized void setProgress(int progress) {
+        setText(progress);
+        super.setProgress(progress);
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Rect rect = new Rect();
+        mPaint.getTextBounds(mText, 0, mText.length(), rect);
+        int x = (getWidth() / 2) - rect.centerX();
+        int y = (getHeight() / 2) - rect.centerY();
+        canvas.drawText(mText, x, y, mPaint);
+    }
+
+    private void initText() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.WHITE);
+    }
+
+    private void setText() {
+        setText(getProgress());
+    }
+
+    private void setText(int progress) {
+        int i = (progress * 100) / getMax();
+        mText = String.valueOf(i) + "%";
+    }
+}
